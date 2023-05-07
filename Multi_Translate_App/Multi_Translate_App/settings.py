92f49972c3fdcfcb57ac6876477aabb388b74293
shi0n0 <shiono.yui1080@gmail.com>
2023-05-08 00:04:46 +0900

    template setting

--- Multi_Translate_App/Multi_Translate_App/settings.py
+++ Multi_Translate_App/Multi_Translate_App/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*l@i4hm5jc0r8e9h3^h^jpkp!zo6r!1m&8=&1(1hsrf3-eb9x'
+SECRET_KEY = 'your_seacret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Multi_Translate_App.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
