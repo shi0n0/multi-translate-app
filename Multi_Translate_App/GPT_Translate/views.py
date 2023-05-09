8e073296cd21dd1e02e45644cac432c1aa1b49b3
shi0n0 <shiono.yui1080@gmail.com>
2023-05-09 23:40:25 +0900

    Delete unnecessary processes

--- Multi_Translate_App/GPT_Translate/views.py
+++ Multi_Translate_App/GPT_Translate/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 import openai
-import os
 
-openai.api_key = os.getenv("APIKEY")
+openai.api_key = "API_KEY"
 
 def translate(input_text):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=(f"Translate the following text from English to Japanese:\n{input_text}\n\n"
+        prompt=(f"You are a professional translator. Translate the following sentences into English in a natural way:\n{input_text}\n\n"
                 "Translation:"),
         temperature=0.7,
         max_tokens=60,
