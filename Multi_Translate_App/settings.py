b773be7acc4569215d5ee773d96cce1dc6904648
shi0n0 <shiono.yui1080@gmail.com>
2023-05-13 16:45:28 +0900

    Fixed a typo in static_url

--- Multi_Translate_App/settings.py
+++ Multi_Translate_App/settings.py
@@ -69,12 +69,6 @@ TEMPLATES = [
     },
 ]
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 
 WSGI_APPLICATION = 'Multi_Translate_App.wsgi.application'
 
@@ -124,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
