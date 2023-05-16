e46b0e5b5099b7ba3733c312925f1d4bd6b2cd99
shi0n0 <shiono.yui1080@gmail.com>
2023-05-16 17:06:14 +0900

    Added English to Japanese translation function

--- GPT_Translate/views.py
+++ GPT_Translate/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import openai
 import os
 
-openai.api_key = os.environ.get('API_KEY')
+openai.api_key = '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'
 
 def translate_jp_to_en(input_text):
     response = openai.Completion.create(
@@ -13,6 +13,17 @@ def translate_jp_to_en(input_text):
         max_tokens=60,
     )
     return response.choices[0].text.strip()
+
+def translate_en_to_jp(input_text):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=(f"You are a professional translator. Translate the following sentences into Japanese in a natural way:\n{input_text}\n\n"
+                "Translation:"),
+        temperature=0.7,
+        max_tokens=60,
+    )
+    return response.choices[0].text.strip()
+
 
 def translation_view(request):
     input_text = ""
