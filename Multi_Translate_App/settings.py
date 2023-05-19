82d4dcf6dd800dbabff84bcd3e765f319387f69f
shi0n0 <shiono.yui1080@gmail.com>
2023-05-20 00:22:20 +0900

    Fix static root

--- Multi_Translate_App/settings.py
+++ Multi_Translate_App/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/multi-translate-app/GPT_Translate/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
