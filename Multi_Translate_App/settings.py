734eda82d2552cc77d32869b2f81907a07e79b3a
shi0n0 <shiono.yui1080@gmail.com>
2023-05-26 19:57:36 +0900

    Add fontawesome

--- Multi_Translate_App/settings.py
+++ Multi_Translate_App/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'GPT_Translate'
+    'GPT_Translate',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
